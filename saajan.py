54f346cc2f4bf9b6ffbf12f48d5c62580387d0c1
Saajan <gursaajan2009@gmail.com>
2024-01-29 15:40:25 +0800

    File handling

--- saajan.py
+++ saajan.py
@@ -1,6 +1,38 @@
-def multiply(a,b):
-    c = a*b
-    print(f"{a}x{b}={c}")
-if __name__=='__main__':
-    multiply(10,20)
+import random
+from turtle import *
+turtle = Turtle('turtle')
+screen = Screen()
+turtle.color('cyan')
+screen.bgcolor('black')
+turtle.pensize(5)
+#color = ['blue','yellow','cyan','purple','red']
+"""def shapes(sides):
+  for j in range(4,sides+i):
+    turtle.pencolor(random.choice(color))
+    turtle.forward(100)
+    turtle.right(360/i)
+for i in range(10,2,-1):
+  shapes(sides=i)
+screen.exitonclick()"""
+"""def spiral():
+  for i in range(0,201):
+    turtle.pencolor(random.choice(color))
+    turtle.circle(i)
+    turtle.speed(i)
+    turtle.right(5)
+spiral()"""
+def spirograph():
+  for i in range(0,201):
+    turtle.speed('fastest')
+    turtle.pencolor(random.choice(color))
+    turtle.circle(100)
+    turtle.setheading(turtle.heading()+i)
+#spirograph()
+turtle.begin_fill()
+for i in range(0,5):
+  turtle.forward(100)
+  turtle.right(72)
+turtle.fillcolor('cyan')
+turtle.end_fill()
 
+turtle.end_fill()
---
+++ spiral s.py
@@ -0,0 +1,5 @@
+def shapes(sides):
+  for i in range(4,11):
+      turtle.forward(100)
+      turtle.right(360/sides)
+shapes(sides = i)
